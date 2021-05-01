268a3c6be7143ed9619bf20410f1061dbc925907
Dejan Popovic <dejan.dp.popovic@gmail.com>
2021-05-01 19:22:50 +0200

    implementing countColumnOccurances()

--- MvcMovie/Models/DataSet.cs
+++ MvcMovie/Models/DataSet.cs
@@ -10,6 +10,7 @@ namespace MvcMovie.Models
     {
         DataTable dt = new DataTable();
 
+        //By convention, the last column in the table is the decision
         public void addAttribute(String attribute) {
             dt.Columns.Add(attribute, typeof(String));// We can also create a row object and Add it as that object in which case Add is overloaded with that signature
         }
@@ -30,16 +31,110 @@ namespace MvcMovie.Models
             return dr;
         }
 
-
-        public void generateDecisionTree() { 
+        public void generateDecisionTree()
+        { 
 
         }
-        public void calcEntropy() { }
+
+
+
+        private Dictionary<string, int> countColumnOccurances(int columnIndex)
+        {
+
+            string indexName = dt.Columns[columnIndex].ColumnName;
+            DataView view = new DataView(dt);
+            DataTable distinctColumnDT = view.ToTable(true, dt.Columns[columnIndex].ColumnName);
+            List<String> listOfDistinctItems = new List<String>();
+            foreach (DataRow r in distinctColumnDT.Rows)
+            {
+                listOfDistinctItems.Add(r[indexName].ToString());
+            }
+
+            var itemsAndTheirCount = new Dictionary<String, int>();
+            int count = 0;
+            foreach (var l in listOfDistinctItems)
+            {
+                foreach (DataRow r in dt.Rows)
+                {
+                    if (l == r[indexName].ToString())
+                    {
+                        count++;
+                    }
+                }
+                itemsAndTheirCount.Add(l, count);
+                count = 0;
+            }
+            return itemsAndTheirCount;
+        }
 
 
 
 
-        void addColumn(string attributeName) { }
+
+
+
+
+        //private Dictionary<string, int> decisionCount()
+        //{
+        //    //Make a unique list of Items for the column of interest
+        //    var decisionIndex = dt.Columns.Count - 1;
+        //    string decisionIndexName = dt.Columns[decisionIndex].ColumnName;
+        //    DataView view = new DataView(dt);
+        //    DataTable distinctDecisionsDT = view.ToTable(true, dt.Columns[decisionIndex].ColumnName);
+        //    List<String> listOfDecisions = new List<String>();
+        //    foreach (DataRow r in distinctDecisionsDT.Rows)
+        //    {
+        //        listOfDecisions.Add(r[decisionIndexName].ToString());
+        //    }
+        //    //Associate each of these items with a count
+        //    var decisionsAndTheirCount = new Dictionary<String, int>();
+        //    int count = 0;
+        //    foreach (var l in listOfDecisions)
+        //    {
+        //        foreach (DataRow r in dt.Rows)
+        //        {
+        //            if (l == r[decisionIndexName].ToString())
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        decisionsAndTheirCount.Add(l, count);
+        //        count = 0;
+        //    }
+        //    return decisionsAndTheirCount;
+        //}
+
+        public void calcEntropy()
+        {
+            int entryCount = dt.Rows.Count;
+
+
+
+
+
+
+
+
+            var i = 0;
+            foreach(DataRow r in dt.Rows)
+            {
+                listOfDecisions[i] = (string)r[decisionIndex];
+                i++;
+            }
+            foreach (var l in listOfDecisions)
+            {
+
+            }
+
+        }
+
+        public double calcEntropyTerm(int totalEntryCount, int specificDecisionCount) 
+        {
+            double r = (double)specificDecisionCount / totalEntryCount;
+            return -r * Math.Log(r, (double)2);
+        }
+
+
 
 
 
