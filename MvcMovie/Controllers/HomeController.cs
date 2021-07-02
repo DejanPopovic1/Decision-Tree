f05c672b1e2612d9bd1ff737edc266c5bd16adfc
Dejan Dejan <dejan.dp.popovic@gmail.com>
2021-07-03 01:22:22 +0200

    control submit event (not working)

--- MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMovie.Models;
 
 namespace MvcMovie.Controllers
 {
@@ -15,6 +16,15 @@ namespace MvcMovie.Controllers
 
         public ActionResult DecisionTree()
         {
+            ViewBag.Message = "How to manage a decision tree";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DecisionTree(DataSet ds)
+        {
+            System.Environment.Exit(-1);
             ViewBag.Message = "How to manage a decision tree";
 
             return View();
